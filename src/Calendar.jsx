b5fed8d6ff1cf7c528996410dc121cea42d1f373
dinesjo <dinesjo@kth.se>
2023-09-30 16:23:49 +0200

    add confirm-modal when moving DL using calendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,12 +1,14 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { Box } from "@mui/joy";
-import { Tooltip } from "@mui/joy";
+import { Box, Button, ModalDialog, Typography } from "@mui/joy";
+import { Tooltip, Modal } from "@mui/joy";
 import DeadlineCard from "./components/DeadlineCard";
+import { useState } from "react";
 
 export default function Calendar({ deadlines, setDeadlines, courses }) {
-  // console.log(deadlines);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [eventInfo, setEventInfo] = useState(null);
   const events = deadlines.map((deadline) => {
     return {
       id: deadline.id,
@@ -20,7 +22,50 @@ export default function Calendar({ deadlines, setDeadlines, courses }) {
     };
   });
 
-  const eventContent = (eventInfo) => {
+  return (
+    <Box>
+      <ConfirmModal
+        setDeadlines={setDeadlines}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        eventInfo={eventInfo}
+      />
+      <FullCalendar
+        events={events}
+        eventContent={eventContent}
+        longPressDelay={250} // how long to wait before dragging on mobile (ms)
+        plugins={[dayGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: "today",
+          center: "title",
+          right: "prev,next",
+        }}
+        titleFormat={{ year: "numeric", month: "short" }}
+        initialView="dayGridMonth"
+        dayHeaderClassNames={["day-header"]}
+        firstDay={1} // start week on Monday
+        weekNumbers={true}
+        weekText="" // remove "W"-prefix
+        weekNumberClassNames={["week-number"]}
+        dayCellClassNames={["day-cell"]}
+        editable={true}
+        eventDurationEditable={false}
+        eventDrop={(eventInfo) => {
+          setEventInfo(eventInfo);
+          setModalOpen(true);
+        }}
+        height="auto"
+      />
+    </Box>
+  );
+
+  /**
+   * Custom event content
+   * @param {Object} eventInfo - event info from FullCalendar
+   * @returns {JSX.Element} - JSX element to render
+   * @see https://fullcalendar.io/docs/eventContent
+   */
+  function eventContent(eventInfo) {
     const deadline = eventInfo.event.extendedProps.deadline;
     return (
       <Tooltip
@@ -49,44 +94,78 @@ export default function Calendar({ deadlines, setDeadlines, courses }) {
         </Box>
       </Tooltip>
     );
-  };
+  }
+}
 
+/**
+ * Modal to confirm moving a deadline
+ * @param {function} setDeadlines - function to set deadlines
+ * @param {Object} eventInfo - event info from FullCalendar
+ * @param {boolean} modalOpen - state of modal
+ * @param {function} setModalOpen - function to set modal state
+ */
+function ConfirmModal({ setDeadlines, eventInfo, modalOpen, setModalOpen }) {
   return (
-    <Box
-    // sx={{
-    //   backgroundColor: "background.body",
-    //   color: "text.black",
-    // }}
+    <Modal
+      open={modalOpen}
+      onClose={() => {
+        setModalOpen(false);
+      }}
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
     >
-      <FullCalendar
-        events={events}
-        eventContent={eventContent}
-        longPressDelay={250} // how long to wait before dragging on mobile (ms)
-        plugins={[dayGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: "today",
-          center: "title",
-          right: "prev,next",
-        }}
-        titleFormat={{ year: "numeric", month: "short" }}
-        initialView="dayGridMonth"
-        dayHeaderClassNames={["day-header"]}
-        firstDay={1} // start week on Monday
-        weekNumbers={true}
-        weekText="" // remove "W"-prefix
-        weekNumberClassNames={["week-number"]}
-        dayCellClassNames={["day-cell"]}
-        editable={true}
-        eventDurationEditable={false}
-        eventDrop={(info) => {
-          const deadline = deadlines.find(
-            (deadline) => deadline.id === info.event.id
-          );
-          deadline.date = info.event.start;
-          setDeadlines([...deadlines]);
-        }}
-        height="auto"
-      />
-    </Box>
+      <ModalDialog>
+        <Typography level="h4">Are you sure?</Typography>
+        <Typography level="body-md" sx={{ mt: 1 }}>
+          Move <strong>{eventInfo?.event.extendedProps.deadline.title}</strong>{" "}
+          to{" "}
+          <strong>
+            {eventInfo?.event.start?.toLocaleDateString("en-US", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
+          </strong>
+          ?
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            gap: ".5em",
+            mt: 2,
+          }}
+        >
+          <Button
+            color="neutral"
+            variant="outlined"
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="solid"
+            onClick={() => {
+              setModalOpen(false);
+              const deadline = eventInfo.event.extendedProps.deadline;
+              setDeadlines((deadlines) => {
+                const newDate = new Date(eventInfo.event.start);
+                newDate.setDate(newDate.getDate() + 1); // HOTFIX
+                deadline.date = newDate.toISOString().split("T")[0]; // format date to yyyy-MM-dd
+                return deadlines.map((d) => {
+                  if (d.id === deadline.id) {
+                    return deadline;
+                  }
+                  return d;
+                });
+              });
+            }}
+          >
+            Confirm
+          </Button>
+        </Box>
+      </ModalDialog>
+    </Modal>
   );
 }
